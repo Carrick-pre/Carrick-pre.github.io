45b53b4a848c6b8e6de501a30e91b88aaa8f6f0a
Carrick-pre <64624685+Carrick-pre@users.noreply.github.com>
2023-05-03 22:27:19 +0100

    fyc
    fafdwa

--- images/list.py
+++ images/list.py
@@ -5,5 +5,5 @@ files = os.listdir(folder_path)
 with open('output.txt', 'w') as w:
 
     for file_name in files:
-       print(f'<img src="images/Buttons/{file_name}">')
-       w.write(f'<img src="images/Buttons/{file_name}">\n')
+       print(f'<img src="images/buttons/{file_name}">')
+       w.write(f'<img src="images/buttons/{file_name}">\n')
